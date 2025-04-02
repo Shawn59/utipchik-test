b488916e21701981fcf087a4c6da2992b122c743
shawn59 <xxx_respekt.ru@mail.ru>
2025-04-02 19:23:18 +0500

    sheduler_epip - в проц

---
+++ src/_atoms/Button/Button.tsx
@@ -0,0 +1,10 @@
+import { Button, Typography } from '@mui/material';
+import styles from './Button.module.scss';
+
+export const ButtonAtom = () => {
+  return (
+    <div>
+      <Button className={styles.buttonAtom}>{'Ку'}</Button>
+    </div>
+  );
+};
