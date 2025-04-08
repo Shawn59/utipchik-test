7158725118ceb3a32f32e62d874e5fc631805e88
shawn59 <xxx_respekt.ru@mail.ru>
2025-04-08 07:58:39 +0500

    sheduler_epip - Добавил в футер сайд меню: - ФИО юзера - Роль юзера - Версию проекта - кнопку разлогирования

---
+++ settings/config.js
@@ -0,0 +1,8 @@
+{
+    "adminSite": "https://stage.troika-gorod.ru/",
+    "adminSiteProd": "https://api.troika-gorod.ru/",
+    "api": {
+        "proxy": "/api"
+    },
+    "version": "v1.7.0"
+}
